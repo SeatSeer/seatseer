a37d0502315160c0324521c56e56c02ae17e6bdb
Bryan Kwok <bryankwokyf@gmail.com>
2021-06-08 10:32:48 +0800

    Moved Start.js into src

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import store from './store/store';
 import { Provider } from 'react-redux';
-import Start from './Start';
+import Start from './src/Start';
 
 export default function App() {
   return (
